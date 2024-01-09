c099df6c80c54f4cf5fc4fd4b76487848d5914bf
TuyenH19 <thtuyen199@gmail.com>
2024-01-09 09:11:48 +0000

    go through a binary tree using pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	else if (tree != NULL && func != NULL)
 	{
-		func(tree->n);
+		printf("%d\n", tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
