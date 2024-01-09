fc2a02e059a6c0f2ee6761c1d37827c4802e5577
HolbertonGab <7290@holbertonstudents.com>
2024-01-09 10:15:21 +0000

    slight changes

--- binary_trees.h
+++ binary_trees.h
@@ -20,7 +20,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}binary_tree_t;
+} binary_tree_t;
+
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -45,3 +46,4 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
 #endif /* _BINARY_TREES_H_ */
+
