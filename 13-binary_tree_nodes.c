37805f68c5b88b38ede9b11a44c9b79b546d5b42
HolbertonGab <7290@holbertonstudents.com>
2024-01-10 12:21:24 +0000

    fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,22 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: Pointer to the root node of the tree to count the number of nodes.
  *
- * @tree: Pointer to the root node of the tree to count the number of nodes
- *
- * Return: The number of nodes with at least one child.
- * If tree is NULL, return 0.
- */
+ * Return: The number of nodes of the tree.
+*/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	size_t c = 0;
+	if ((tree->left != NULL) || (tree->right != NULL))
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
+		+ 1);
 
-	if (tree->left != NULL || tree->right != NULL)
-		c = 1;
-
-	return (c + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,19 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the height.
  *
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree. If tree is NULL, return 0.
- */
+ * Return: The number of height of the tree.
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	size_t height_left = 0, height_right = 0;
+
+	if ((tree == NULL) || ((tree->left == NULL) && (tree->right == NULL)))
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	if (height_left > height_right)
+		return (height_left + 1);
+	else
+		return (height_right + 1);
 }
